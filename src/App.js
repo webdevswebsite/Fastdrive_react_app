cab8af3492592cc3326ff1f9c7dc0567363b36f0
olu-martins <mayowasamuel86@gmail.com>
2022-11-23 12:03:59 +0100

    base name

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ const ScrollToTop = withRouter(({ children, location: { pathname } }) => {
 
 function App() {
   return (
-    <Router basename={"/tm/react/toor/"}>
+    <Router basename={"/"}>
       <Suspense fallback={<div></div>}>
         <ScrollToTop>
           {/* <Preloader /> */}
